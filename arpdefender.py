8b7c9da81ef0f1a5fb41f5aae88f6756c54da144
MrFrost <89649789+nrgaig@users.noreply.github.com>
2025-11-26 10:53:06 +0200

    arpdefender v0.7

--- arpdefender.py
+++ arpdefender.py
@@ -3,7 +3,7 @@ from scapy.all import *
 balance = 0
 ip_list={}
 
-def duplicate_arp_packets(packet):
+def duplicate_arp_packets(packet): # detects duplicate MAC address for different IPs
     src_ip = ""
     src_mac = ""
     if ARP in packet:
@@ -12,11 +12,11 @@ def duplicate_arp_packets(packet):
             src_ip = packet[ARP].psrc
         if src_ip in ip_list:
             if ip_list[src_ip] != src_mac:
-                print("Duplicate arp detected")
+                print("WARNING: Duplicate arp detected")
         else:
             ip_list[src_ip] = src_mac
 
-def free_arp_packets(packet):
+def free_arp_packets(packet): # detects IS-AT packets which were not asked for
     global balance
     if ARP in packet:
         if packet[ARP].op == 1:
@@ -25,17 +25,16 @@ def free_arp_packets(packet):
             balance -= 1
 
         if balance < -3:
-            print("Arp spoofing detected")
+            print("WARNING: Free ARP packets detected")
 
 
 
-def arp_filter(packet):
+def arp_filter(packet): # call for the detection methods
     if ARP in packet:
-
         duplicate_arp_packets(packet)
         free_arp_packets(packet)
 
 
 
-sniff(filter="arp", prn=arp_filter)
+sniff(filter="arp", prn=arp_filter) # sniffing packets and sending to arp_filter
 
